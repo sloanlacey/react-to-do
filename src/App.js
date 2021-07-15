0170e87ef11a155f738c681d08daa6fec6f04411
Sloan Lacey <sloanlacey@Sloans-MBP-2.domain>
2021-07-14 17:09:48 -0700

    react-to-do: 4
    -Set state parameters to allow inputs int he form section
    -Pass that state into the elements from App.js
    -Set submit button to update state on click and clear text in input field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form.js";
 import TodoList from "./components/TodoList/TodoList.js";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Hop to it</h1>
       </header>
-      <Form />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
