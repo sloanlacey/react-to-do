bd776677e2a0cbe662b56160430b802724e618c1
Sloan Lacey <sloanlacey@Sloans-MBP-2.domain>
2021-07-20 16:10:33 -0700

    react-to-do: 6
    -Update completed and delete functionalities
    -At the local storage level this app can create, delete and update list entries

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         inputText={inputText}
         setInputText={setInputText}
       />
-      <TodoList />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Todo from '../Todo/Todo.js'
+import Todo from "../Todo/Todo.js";
 
-const TodoList = () => {
+const TodoList = ({ todos, setTodos }) => {
+  console.log(todos);
   return (
     <div className="todo-container">
       <ul className="todo-list">
-          <Todo />
+        {todos.map((todo) => (
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            key={todo.id}
+            todo={todo}
+            text={todo.text}
+          />
+        ))}
       </ul>
     </div>
   );
